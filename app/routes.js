ce7c84d896820fef3acea70977056fe72a472f34
Blake Lewis <blakejlewis@hotmail.com>
2018-03-29 02:12:08 -0500

    Added functionality for viewing, editing, and deleting Roommate Agreements

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,5 @@
 // app/routes.js
+
 var mysql = require('mysql');
 var bcrypt = require('bcrypt-nodejs');
 
@@ -79,8 +80,9 @@ module.exports = function(app, passport) {
 			lastName: req.body.lastName
 		}
 
-		var insertQuery = ("UPDATE Employee SET password = ?, firstName = ?, lastName = ? WHERE empID = ?");
-		connection.query(insertQuery, [updateProfileMysql.password, updateProfileMysql.firstName, updateProfileMysql.lastName, req.user.empID], function(err, rows){});
+		var updateQuery = ("UPDATE Employee SET password = ?, firstName = ?, lastName = ? WHERE empID = ?");
+		connection.query(updateQuery, [updateProfileMysql.password, updateProfileMysql.firstName, updateProfileMysql.lastName, 
+									   req.user.empID], function(err, rows){});
 		res.redirect('/profile');
 	})
 
@@ -157,9 +159,127 @@ module.exports = function(app, passport) {
 
 		var insertQuery = "INSERT INTO RoommateAgreement ( communityID, caID, roomNumber, roommate1, roommate2, stressor1, stressManagement1, stressHelp1, communicationVia1, stressor2, stressManagement2, stressHelp2, communicationVia2, studyTime, studyActivities, studyAdjustments, weekdaySleeptime, weekendSleeptime, sleepActivities, cleanTasks1, cleanFrequency1, cleanTasks2, cleanFrequency2, belongingPermission, sharedElectronics, sharedClothes, sharedFood, sharedHousehold, sharedOther, whileAway, respectPrivacy, roommate1Habits, roommate1PetPeeves, roommate2Habits, roommate2PetPeeves, guestPermission, guestPrivacy, whenLocked, alcoholDrugs, temperature, damage, roommate1Signature, roommate2Signature ) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
-		connection.query(insertQuery, [newAgreementMysql.communityID, newAgreementMysql.caID, newAgreementMysql.roomNumber, newAgreementMysql.roommate1, newAgreementMysql.roommate2, newAgreementMysql.stressor1, newAgreementMysql.stressManagement1, newAgreementMysql.stressHelp1, newAgreementMysql.communicationVia1, newAgreementMysql.stressor2, newAgreementMysql.stressManagement2, newAgreementMysql.stressHelp2, newAgreementMysql.communicationVia2, newAgreementMysql.studyTime, newAgreementMysql.studyActivities, newAgreementMysql.studyAdjustments, newAgreementMysql.weekdaySleeptime, newAgreementMysql.weekendSleeptime, newAgreementMysql.sleepActivities, newAgreementMysql.cleanTasks1, newAgreementMysql.cleanFrequency1, newAgreementMysql.cleanTasks2, newAgreementMysql.cleanFrequency2, newAgreementMysql.belongingPermission, newAgreementMysql.sharedElectronics, newAgreementMysql.sharedClothes, newAgreementMysql.sharedFood, newAgreementMysql.sharedHousehold, newAgreementMysql.sharedOther, newAgreementMysql.whileAway, newAgreementMysql.respectPrivacy, newAgreementMysql.roommate1Habits, newAgreementMysql.roommate1PetPeeves, newAgreementMysql.roommate2Habits, newAgreementMysql.roommate2PetPeeves, newAgreementMysql.guestPermission, newAgreementMysql.guestPrivacy, newAgreementMysql.whenLocked, newAgreementMysql.alcoholDrugs, newAgreementMysql.temperature, newAgreementMysql.damage, newAgreementMysql.roommate1Signature, newAgreementMysql.roommate2Signature], function(err,rows) {});
+		connection.query(insertQuery, [newAgreementMysql.communityID, newAgreementMysql.caID, newAgreementMysql.roomNumber, newAgreementMysql.roommate1, 
+									   newAgreementMysql.roommate2, newAgreementMysql.stressor1, newAgreementMysql.stressManagement1, 
+									   newAgreementMysql.stressHelp1, newAgreementMysql.communicationVia1, newAgreementMysql.stressor2, 
+									   newAgreementMysql.stressManagement2, newAgreementMysql.stressHelp2, newAgreementMysql.communicationVia2, 
+									   newAgreementMysql.studyTime, newAgreementMysql.studyActivities, newAgreementMysql.studyAdjustments, 
+									   newAgreementMysql.weekdaySleeptime, newAgreementMysql.weekendSleeptime, newAgreementMysql.sleepActivities, 
+									   newAgreementMysql.cleanTasks1, newAgreementMysql.cleanFrequency1, newAgreementMysql.cleanTasks2, 
+									   newAgreementMysql.cleanFrequency2, newAgreementMysql.belongingPermission, newAgreementMysql.sharedElectronics, 
+									   newAgreementMysql.sharedClothes, newAgreementMysql.sharedFood, newAgreementMysql.sharedHousehold, 
+									   newAgreementMysql.sharedOther, newAgreementMysql.whileAway, newAgreementMysql.respectPrivacy, 
+									   newAgreementMysql.roommate1Habits, newAgreementMysql.roommate1PetPeeves, newAgreementMysql.roommate2Habits, 
+									   newAgreementMysql.roommate2PetPeeves, newAgreementMysql.guestPermission, newAgreementMysql.guestPrivacy, 
+									   newAgreementMysql.whenLocked, newAgreementMysql.alcoholDrugs, newAgreementMysql.temperature, newAgreementMysql.damage, 
+									   newAgreementMysql.roommate1Signature, newAgreementMysql.roommate2Signature], function(err,rows) {
+									   	if(err) throw err;
+									   });
 		res.redirect(302, '/agreement');	
 	});
+
+	app.get('/viewAgreement', isLoggedIn, function(req, res) {
+		var query = "SELECT * FROM RoommateAgreement";
+		connection.query(query, function(err, rows, fields) {
+			if (err) throw err;
+
+			res.render('viewAgreement.ejs', {
+				Employee: req.user,
+				Agreements: rows
+			});
+		});
+	});
+
+	app.get('/editAgreement/:id', isLoggedIn, function(req, res) {
+		let agreementID = req.params.id;
+		var query = ("SELECT * FROM RoommateAgreement WHERE agreementID = ?");
+		connection.query(query, agreementID, function(err, rows, fields){
+			if (err) throw err;
+
+			res.render('editAgreement.ejs', {
+				Employee: req.user,
+				Agreement: rows
+			});
+		});
+	});
+
+	app.post('/updateAgreement/:id', isLoggedIn, function(req, res) {
+		let agreementID = req.params.id;
+		var updateAgreementMysql = {
+			communityID: req.user.communityID,
+			caID: req.user.empID,
+			roomNumber: req.body.roomNumber,
+			roommate1: req.body.roommate1,
+			roommate2: req.body.roommate2,
+			stressor1: req.body.stressor1,
+			stressManagement1: req.body.stressManagement1,
+			stressHelp1: req.body.stressHelp1,
+			communicationVia1: req.body.communicationVia1,
+			stressor2: req.body.stressor2,
+			stressManagement2: req.body.stressManagement2,
+			stressHelp2: req.body.stressHelp2,
+			communicationVia2: req.body.communicationVia2,
+			studyTime: req.body.studyTime,
+			studyActivities: req.body.studyActivities,
+			studyAdjustments: req.body.studyAdjustments,
+			weekdaySleeptime: req.body.weekdaySleeptime,
+			weekendSleeptime: req.body.weekendSleeptime,
+			sleepActivities: req.body.sleepActivities,
+			cleanTasks1: req.body.cleanTasks1,
+			cleanFrequency1: req.body.cleanFrequency1,
+			cleanTasks2: req.body.cleanTasks2,
+			cleanFrequency2: req.body.cleanFrequency2,
+			belongingPermission: req.body.belongingPermission,
+			sharedElectronics: req.body.sharedElectronics,
+			sharedClothes: req.body.sharedClothes,
+			sharedFood: req.body.sharedFood,
+			sharedHousehold: req.body.sharedHousehold,
+			sharedOther: req.body.sharedOther,
+			whileAway: req.body.whileAway,
+			respectPrivacy: req.body.respectPrivacy,
+			roommate1Habits: req.body.roommate1Habits,
+			roommate1PetPeeves: req.body.roommate1PetPeeves,
+			roommate2Habits: req.body.roommate2Habits,
+			roommate2PetPeeves: req.body.roommate2PetPeeves,
+			guestPermission: req.body.guestPermission,
+			guestPrivacy: req.body.guestPrivacy,
+			whenLocked: req.body.whenLocked,
+			alcoholDrugs: req.body.alcoholDrugs,
+			temperature: req.body.temperature,
+			damage: req.body.damage,
+			roommate1Signature: req.body.roommate1Signature,
+			roommate2Signature: req.body.roommate2Signature
+		};
+
+		var updateQuery = ("UPDATE RoommateAgreement SET communityID = ?, caID = ?, roomNumber = ?, roommate1 = ?, roommate2 = ?, stressor1 = ?, stressManagement1 = ?, stressHelp1 = ?, communicationVia1 = ?, stressor2 = ?, stressManagement2 = ?, stressHelp2 = ?, communicationVia2 = ?, studyTime = ?, studyActivities = ?, studyAdjustments = ?, weekdaySleeptime = ?, weekendSleeptime = ?, sleepActivities = ?, cleanTasks1 = ?, cleanFrequency1 = ?, cleanTasks2 = ?, cleanFrequency2 = ?, belongingPermission = ?, sharedElectronics = ?, sharedClothes = ?, sharedFood = ?, sharedHousehold = ?, sharedOther = ?, whileAway = ?, respectPrivacy = ?, roommate1Habits = ?, roommate1PetPeeves = ?, roommate2Habits = ?, roommate2PetPeeves = ?, guestPermission = ?, guestPrivacy = ?, whenLocked = ?, alcoholDrugs = ?, temperature = ?, damage = ?, roommate1Signature = ?, roommate2Signature = ? WHERE agreementID = ?");
+		connection.query(updateQuery, [updateAgreementMysql.communityID, updateAgreementMysql.caID, updateAgreementMysql.roomNumber, updateAgreementMysql.roommate1, 
+									   updateAgreementMysql.roommate2, updateAgreementMysql.stressor1, updateAgreementMysql.stressManagement1, 
+									   updateAgreementMysql.stressHelp1, updateAgreementMysql.communicationVia1, updateAgreementMysql.stressor2, 
+									   updateAgreementMysql.stressManagement2, updateAgreementMysql.stressHelp2, updateAgreementMysql.communicationVia2, 
+									   updateAgreementMysql.studyTime, updateAgreementMysql.studyActivities, updateAgreementMysql.studyAdjustments, 
+									   updateAgreementMysql.weekdaySleeptime, updateAgreementMysql.weekendSleeptime, updateAgreementMysql.sleepActivities, 
+									   updateAgreementMysql.cleanTasks1, updateAgreementMysql.cleanFrequency1, updateAgreementMysql.cleanTasks2, 
+									   updateAgreementMysql.cleanFrequency2, updateAgreementMysql.belongingPermission, updateAgreementMysql.sharedElectronics, 
+									   updateAgreementMysql.sharedClothes, updateAgreementMysql.sharedFood, updateAgreementMysql.sharedHousehold, 
+									   updateAgreementMysql.sharedOther, updateAgreementMysql.whileAway, updateAgreementMysql.respectPrivacy, 
+									   updateAgreementMysql.roommate1Habits, updateAgreementMysql.roommate1PetPeeves, updateAgreementMysql.roommate2Habits, 
+									   updateAgreementMysql.roommate2PetPeeves, updateAgreementMysql.guestPermission, updateAgreementMysql.guestPrivacy, 
+									   updateAgreementMysql.whenLocked, updateAgreementMysql.alcoholDrugs, updateAgreementMysql.temperature, updateAgreementMysql.damage, 
+									   updateAgreementMysql.roommate1Signature, updateAgreementMysql.roommate2Signature, agreementID], function(err,rows) {
+
+									   	if(err) throw err;});
+		res.redirect('/agreement');
+	});
+
+	app.post('/deleteAgreement/:id', isLoggedIn, function(req, res) {
+		var agreementID = req.params.id;
+		var deleteQuery = ("DELETE FROM RoommateAgreement WHERE agreementID = ?");
+		connection.query(deleteQuery, agreementID, function(err, rows) {
+			if(err) throw err;
+			res.redirect(302, '/agreement');
+		});
+	});
+
 
 	app.get('/proposal', isLoggedIn, function(req, res) {
 		// render the page and pass in any flash data if it exists
